95436a872fd4b41550cf8899520632bd74ef3f90
chenhy-1999 <1103577176@qq.com>
2020-06-03 17:59:17 +0800

    springcloud

---
+++ producer/src/main/java/com/example/producer/ProducerController.java
@@ -0,0 +1,11 @@
+package com.example.producer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class ProducerController {
+    @RequestMapping("/producer/haha")
+    public String produce(){
+        return "Hello World";
+    }
+}
